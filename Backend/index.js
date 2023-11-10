449285c556484f685879571e57f0525b1f370fc8
BaqerAlabedi <112864227+BaqerAlabedi@users.noreply.github.com>
2023-11-10 22:27:19 +0100

    finals

--- Backend/index.js
+++ Backend/index.js
@@ -23,6 +23,18 @@ app.options('*', cors({
   credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://savastano-milano-nb4r.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // For preflight requests
+  } else {
+    next();
+  }
+});
+
 app.use(require("./routes/auth"));
 
 mongoose.connect(MONGOURL, {
